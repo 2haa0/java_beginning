12c3d0042e8cddf2bb52fd879fcba0b0887ccf0d
2haa0 <86291449+2haa0@users.noreply.github.com>
2021-06-22 18:04:56 +0900

    Add files via upload

---
+++ day00/src/start/Hello.java
@@ -0,0 +1,18 @@
+package start; //맨위에는 항상 packge가 위치해야한다.
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// main함수에 처리내용을 넣어보자.
+
+		System.out.println("나는 자동완성입니다.");
+		
+		System.out.println("환영합니다."); 
+		// syso, sisout + ctrl + spacebar
+		// 처음.은 '어디에?' 모니터에 출력해야하므로 out, 두번째 .은 '무슨 기능?' 프린트는 패키지이므로 소괄호
+		//활성화 된 .java 탭 앞에 *가 있으면 아직 저장 안 된 상태
+		System.out.print("반갑습니다."); 
+
+	}
+
+}
